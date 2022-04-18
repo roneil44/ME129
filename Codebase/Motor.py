8646ba8e112f53cc3e88e03dfc932416aede03d8
Grace Ding <grace.ding1999@gmail.com>
2022-04-18 15:41:24 -0700

    added initialization to motor class

--- Codebase/Motor.py
+++ Codebase/Motor.py
@@ -1,9 +1,56 @@
 # imports
+import pigpio
+import sys
+import time
+
+# Define the motor pins.
+MTR1_LEGA = 7
+MTR1_LEGB = 8
+
+MTR2_LEGA = 5
+MTR2_LEGB = 6
 
 class Motor:
 
     def __init__(self, name:str):
         self.name = name
+        ############################################################
+        # Prepare the GPIO connetion (to command the motors).
+        print("Setting up the GPIO...")
+
+        # Initialize the connection to the pigpio daemon (GPIO interface).
+        self.io = pigpio.pi()
+        if not self.io.connected:
+            print("Unable to connection to pigpio daemon!")
+            sys.exit(0)
+
+        # Set up the four pins as output (commanding the motors).
+        self.io.set_mode(MTR1_LEGA, pigpio.OUTPUT)
+        self.io.set_mode(MTR1_LEGB, pigpio.OUTPUT)
+        self.io.set_mode(MTR2_LEGA, pigpio.OUTPUT)
+        self.io.set_mode(MTR2_LEGB, pigpio.OUTPUT)
+
+        # Prepare the PWM.  The range gives the maximum value for 100%
+        # duty cycle, using integer commands (1 up to max).
+        self.io.set_PWM_range(MTR1_LEGA, 255)
+        self.io.set_PWM_range(MTR1_LEGB, 255)
+        self.io.set_PWM_range(MTR2_LEGA, 255)
+        self.io.set_PWM_range(MTR2_LEGB, 255)
+        
+        # Set the PWM frequency to 1000Hz.  You could try 500Hz or 2000Hz
+        # to see whether there is a difference?
+        self.io.set_PWM_frequency(MTR1_LEGA, 1000)
+        self.io.set_PWM_frequency(MTR1_LEGB, 1000)
+        self.io.set_PWM_frequency(MTR2_LEGA, 1000)
+        self.io.set_PWM_frequency(MTR2_LEGB, 1000)
+
+        # Clear all pins, just in case.
+        self.io.set_PWM_dutycycle(MTR1_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR1_LEGB, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGB, 0)
+
+        print("GPIO ready...")
 
     def move(self, speed:int):
         #Detects if movement direction should be forward or backward and then calls the motor for movement
