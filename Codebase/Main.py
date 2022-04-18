4561f7e3b1d29bc42adaba34e80335d47e94a4de
Grace Ding <grace.ding1999@gmail.com>
2022-04-18 16:12:39 -0700

    fixed minor syntax error

--- Codebase/Main.py
+++ Codebase/Main.py
@@ -8,10 +8,12 @@ from .Motor import Motor
 
 ## Main Body
 
-if __name__ == '__main_':
-    motor = Motor()
+if __name__ == '__main__':
     try:
         # Robot execution code # 
+        motor = Motor()
+        print('moto initialized"')
+        #move left wheel forward
         motor.set(0.2, 0)
         motor.shutdown()
     except: 
