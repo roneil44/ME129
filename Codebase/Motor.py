f6f13b06b6b5caf90560310f668011c25862a3f7
Riley O'Neil <rileyponeil@yahoo.com>
2022-04-25 16:29:12 -0700

    Fixed turning issue

--- Codebase/Motor.py
+++ Codebase/Motor.py
@@ -144,12 +144,12 @@ class Motor:
     def setvel(self, vel: float, w: float, duration:float):
         # Takes a straight speed (m/s), rotation speed (deg/s), and time. Then moves the robot the superposition of the two inputs
         dutystraight = (vel+.156) / .624
-        dutyturn = (w + 157) / 500
+        dutyturn = (abs(w) + 157) / 500
         if w >= 0:
             left = dutystraight + dutyturn
             right = dutystraight - dutyturn
             self.move(left, right, duration)
-        elif w< 0:
+        elif w < 0:
             left = dutystraight - dutyturn
             right = dutystraight + dutyturn
             self.move(left, right, duration)
