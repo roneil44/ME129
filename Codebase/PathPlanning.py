4f858e667bde1a96bfe31beef621060bfd46cba8
Isaac <isaac.j.perrin@gmail.com>
2022-05-10 17:39:49 -0700

    changed path planning to functions

--- Codebase/PathPlanning.py
+++ Codebase/PathPlanning.py
@@ -14,6 +14,118 @@ import random
 # The value is a list of available paths and a list of whether they have been explored
 
 
+def pointToPointDirections(Map, StartingPosition, Destination):
+    # Directions dictionary holds all possible routes to the destination
+    # Key is where each path currently ends and key value is the list of turns to get there
+    
+    # Directions is initialized with starting position and an empty list of turns to get there
+    Directions = {StartingPosition: []}
+    
+    # variable to indicate route search is still in progress
+    searching = TRUE
+
+    # keeps list of coordinates that pathes have already crosses so pathes won't
+    # backtrack or follow other redundant pathes
+    coordsMapped = [StartingPosition]
+
+    #main loop that propogates the dictionary
+    while searching == TRUE:
+        
+        # iterate through all current paths extending them to new valid coordinates
+        # saves to new dictionary of directions
+        newDirections = {}
+
+        # loop starts with every known path
+        for currentCoords in Directions.keys():
+            
+            # loads map data at this point
+            connections = Map.get(currentCoords)[0]
+            explored    = Map.get(currentCoords)[1]
+            
+            # for loop will try and create a new path by extending in available directions
+            # which are existant, already explored, and the where the new coordinate is on the map
+            # but not added to the coordsMapped list (list of coordinates already on existing pathes)
+            for i in range(4):
+                if connections[i] and explored[i]:
+                    newCoordinate = shift(currentCoords,i)
+                    if newCoordinate in coordsMapped:
+                        pass
+                    elif newCoordinate in Map:
+                        newDirections[newCoordinate] = Directions[currentCoords]+[i]
+                        coordsMapped.append(newCoordinate)
+
+        # replace directions dictionary with updated directions one step further        
+        Directions = newDirections
+
+        # checks for all routes being terminated before reaching desintation
+        if len(Directions) == 0:
+            searching = FALSE
+            print("No Route Found")
+
+        # checks for any routes that found the destinations
+        if Destination in Directions.keys():
+            searching = FALSE
+            print("Route Found")
+    
+    #prints output path if available
+    if len(Directions) == 0:
+        pass
+        return([])
+    else:
+        print(Directions.get(Destination))
+        return(Directions.get(Destination))
+    
+def nearestUnexploredDirections(Map, StartingPosition):
+    # Directions dictionary holds all possible routes to the destination
+    # Key is where each path currently ends and key value is the list of turns to get there
+    
+    # Directions is initialized with starting position and an empty list of turns to get there
+    Directions = {StartingPosition: []}
+    
+    # variable to indicate route search is still in progress
+    searching = TRUE
+
+    # keeps list of coordinates that pathes have already crosses so pathes won't
+    # backtrack or follow other redundant pathes
+    coordsMapped = [StartingPosition]
+
+    #main loop that propogates the dictionary
+    while searching == TRUE:
+        
+        # iterate through all current paths extending them to new valid coordinates
+        # saves to new dictionary of directions
+        newDirections = {}
+
+        # loop starts with every known path
+        for currentCoords in Directions.keys():
+            
+            # loads map data at this point
+            connections = Map.get(currentCoords)[0]
+            explored    = Map.get(currentCoords)[1]
+
+            if False in explored:
+                return(Directions.get(currentCoords))
+            
+            # for loop will try and create a new path by extending in available directions
+            # which are existant, already explored, and the where the new coordinate is on the map
+            # but not added to the coordsMapped list (list of coordinates already on existing pathes)
+            for i in range(4):
+                if connections[i] and explored[i]:
+                    newCoordinate = shift(currentCoords,i)
+                    if newCoordinate in coordsMapped:
+                        pass
+                    elif newCoordinate in Map:
+                        newDirections[newCoordinate] = Directions[currentCoords]+[i]
+                        coordsMapped.append(newCoordinate)
+
+        # replace directions dictionary with updated directions one step further        
+        Directions = newDirections
+
+        # checks for all routes being terminated before reaching a unopen street
+        if len(Directions) == 0:
+            searching = FALSE
+            print("No Unexplored Street Found")
+        
     
 def shift(coords, Direction):
     # Update longitude/latitude value after a step in the given heading.
@@ -41,84 +153,42 @@ def shift(coords, Direction):
         raise Exception("Error in shift")
 
 
-## Main Body
+## Main Body used for testing
 if __name__ == '__main__':
 
-    Map = {\
-        (0, 0):[[True, False, False, True], [True, False, False, True]], \
-        (1, 0):[[True, True, False, True], [True, True, False, True]], \
-        (2, 0):[[True, True, False, False], [True, True, False, False]], \
-        (0, 1):[[True, False, True, False], [True, False, True, False]], \
-        (1, 1):[[False, False, True, True], [False, False, True, True]], \
-        (2, 1):[[True, True, True, False], [True, True, True, False]], \
-        (0, 2):[[False, False, True, True], [False, False, True, True]], \
-        (1, 2):[[True, True, False, True], [True, True, False, True]], \
-        (2, 2):[[True, True, True, False], [True, True, True, False]], \
-        (0, 3):[[True, False, False, True], [True, False, False, True]], \
+    FullMap = {\
+        (0, 0):[[True, False, False, True], [True, True, True, True]], \
+        (1, 0):[[True, True, False, True], [True, True, True, True]], \
+        (2, 0):[[True, True, False, False], [True, True, True, True]], \
+        (0, 1):[[True, False, True, False], [True, True, True, True]], \
+        (1, 1):[[False, False, True, True], [True, True, True, True]], \
+        (2, 1):[[True, True, True, False], [True, True, True, True]], \
+        (0, 2):[[False, False, True, True], [True, True, True, True]], \
+        (1, 2):[[True, True, False, True], [True, True, True, True]], \
+        (2, 2):[[True, True, True, False], [True, True, True, True]], \
+        (0, 3):[[True, False, False, True], [True, True, True, True]], \
         (1, 3):[[True, True, True, True], [True, True, True, True]], \
-        (2, 3):[[False, True, True, False], [False, True, True, False]], \
-        (0, 4):[[False, False, True, True], [False, False, True, True]], \
-        (1, 4):[[False, True, True, False], [False, True, True, False]]}
+        (2, 3):[[False, True, True, False], [True, True, True, True]], \
+        (0, 4):[[False, False, True, True], [True, True, True, True]], \
+        (1, 4):[[False, True, True, False], [True, True, True, True]]}
 
-    StartingPosition = (0,0)
-    Destination = (0,4)
+    IncompleteMap = {\
+        (0, 0):[[True, False, False, True], [True, True, True, True]], \
+        (1, 0):[[True, True, False, True], [True, True, True, True]], \
+        (2, 0):[[True, True, False, False], [True, True, True, True]], \
+        (0, 1):[[True, False, True, False], [True, True, True, True]], \
+        (1, 1):[[False, False, True, True], [True, True, True, True]], \
+        (2, 1):[[True, True, True, False], [True, True, True, True]], \
+        (0, 2):[[False, False, True, True], [True, True, True, True]], \
+        (1, 2):[[True, True, False, True], [True, True, True, True]], \
+        (2, 2):[[True, True, True, False], [True, True, True, True]], \
+        (0, 3):[[True, True, False, True], [True, False, True, True]], \
+        (1, 3):[[True, True, True, True], [True, True, True, True]], \
+        (2, 3):[[False, True, True, False], [True, True, True, True]], \
+        (0, 4):[[False, False, True, True], [True, True, True, True]], \
+        (1, 4):[[False, True, True, False], [True, True, True, True]]}
 
-    # Directions dictionary holds all possible routes to the destination
-    # Key is where each path currently ends and key value is the list of turns to get there
+    pointToPointDirections(FullMap,(0,4),(1,4))
+    print(nearestUnexploredDirections(IncompleteMap,(1,3)))
+
     
-    # Directions is initialized with starting position an empty list of turns to get there
-    Directions = {StartingPosition: []}
-    
-    # variable to indicate route search is still in progress
-    searching = TRUE
-
-    # keeps list of coordinates that pathes have already crosses so pathes won't
-    # backtrack or follow other redundant pathes
-    coordsTraveled = [StartingPosition]
-
-    #main loop that propogates the dictionary
-    while searching == TRUE:
-        
-        # iterate through all current paths extending them to new valid coordinates
-        # saves to new dictionary of directions
-        newDirections = {}
-
-        # loop starts with every known path
-        for currentCoords in Directions.keys():
-            
-            # loads map data at this point
-            connections = Map.get(currentCoords)[0]
-            explored    = Map.get(currentCoords)[1]
-            
-            # for loop will try and create a new path by extending in available directions
-            # which are existant, already explored, and the where the new coordinate hasn't
-            # been added to the coordsTraveled list (list of coordinates already on existing pathes)
-            for i in range(4):
-                if connections[i] and explored[i]:
-                    newCoordinate = shift(currentCoords,i)
-                    if newCoordinate in coordsTraveled:
-                        pass
-                    else:
-                        newDirections[newCoordinate] = Directions[currentCoords]+[i]
-                        coordsTraveled.append(newCoordinate)
-
-        # replace directions dictionary with updated directions one step further        
-        Directions = newDirections
-
-        print(Directions)
-
-        # checks for all routes being terminated before reaching desintation
-        if len(Directions) == 0:
-            searching = FALSE
-            print("No Route Found")
-
-        # checks for any routes that found the destinations
-        if Destination in Directions.keys():
-            searching = FALSE
-            print("Route Found")
-    
-    #prints output path if available
-    if len(Directions) == 0:
-        pass
-    else:
-        print(Directions.get(Destination))
